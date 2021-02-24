af6a1af88f55456b48d30449f8788dcabed54f61
emmanuel23dms <emmanuel23dms@gmail.com>
2021-02-24 13:09:28 -0600

    clase del 24 de febrero del 2020

---
+++ sysinform2809/index2.js
@@ -0,0 +1,21 @@
+var Http = require( 'http' );
+var fs = require('fs');
+var server = Http.createServer(function(request,response) {
+
+
+    fs.readFile('hola.html',function(err,datos){
+      console.log('Alguien entró');
+      response.Write(datos);
+      response.end();
+    });
+
+
+
+
+
+
+});
+
+server.listen( 3000, function( ) {
+console.log( 'Escuchando conexión en el puerto 3000' );
+});
